c2c265ee2ec91a8d815bb4420ae9d14c1a33f34c
sari-dev-1 <a0583214067@gmail.com>
2025-07-20 23:42:18 +0300

    Create PhysicsFactory for generating appropriate Physics instances based on command

--- It1_interfaces/PhysicsFactory.py
+++ It1_interfaces/PhysicsFactory.py
@@ -1,12 +1,17 @@
+from typing import Tuple, Dict
+from Command import Command
 from Board import Board
-from Physics import Physics
+from Physics import Physics, IdlePhysics, MovePhysics
 
+class PhysicsFactory:
+    def __init__(self, board: Board):
+        self.board = board
 
-class PhysicsFactory:      # very light for now
-    def __init__(self, board: Board): 
-        """Initialize physics factory with board."""
-        pass
-        
-    def create(self, start_cell, cfg) -> Physics:
-        """Create a physics object with the given configuration."""
-        pass 
+    def create(self, start_cell: Tuple[int, int], cmd: Command, cfg: Dict) -> Physics:
+        """Create a Physics instance (Idle or Move) based on command type."""
+        speed = cfg.get("speed_m_s", 1.0)
+
+        if cmd.type == "move":
+            return MovePhysics(start_cell, self.board, speed)
+        else:
+            return IdlePhysics(start_cell, self.board, speed)
